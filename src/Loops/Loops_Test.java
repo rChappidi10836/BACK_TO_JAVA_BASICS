2a220e499bfa208da13b9dee2579f949737d3054
Raja <rajasrinivas1596@gmail.com>
2026-04-07 12:17:54 +0530

    prime code explaination in loops

--- src/Loops/Loops_Test.java
+++ src/Loops/Loops_Test.java
@@ -3,6 +3,16 @@ package Loops;
 public class Loops_Test {
 
     public boolean findPrimeorNot(int n) {
+        /**
+         * How the below code works by functional pairing we don't have check the factors twice
+         * for example if we check if 36 is divisible by 2 or not then we are automatically checking if 18 is divisible or not too
+         * cuz even 2*18 or 18*2 don't change
+         *
+         * so what we are doinng is taking the sqrt of the number and checking if it has any factor in between 1 and sqrt(n)
+         * if yes it will be non-prime and no it will be prime cuz
+         * prime only have 1 and itself(n) as factors
+         *
+         * */
         int count = 0;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             //System.out.println("n%i => " + n + "%" + i + " = " + (n % i));
