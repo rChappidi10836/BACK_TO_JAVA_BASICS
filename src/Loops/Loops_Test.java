78a29500a8aad54819fd019a872b17e9f71df233
Raja <rajasrinivas1596@gmail.com>
2026-04-09 09:45:25 +0530

    factorial

--- src/Loops/Loops_Test.java
+++ src/Loops/Loops_Test.java
@@ -5,7 +5,7 @@ public class Loops_Test {
     public boolean findPrimeorNot(int n) {
         /**
          * How the below code works by functional pairing we don't have check the factors twice
-         * for example if we check if 36 is divisible by 2 or not then we are automatically checking if 18 is divisible or not too
+         * for example if we check is 36 is divisible by 2 or not then we are automatically checking if 18 is divisible or not too
          * cuz even 2*18 or 18*2 don't change
          *
          * so what we are doinng is taking the sqrt of the number and checking if it has any factor in between 1 and sqrt(n)
@@ -22,12 +22,44 @@ public class Loops_Test {
         }
         return true;
     }
+
+//    static void findPrimes1toN(int N){
+//        int[] arr= new int[];
+//        for(int i=1;i<=N;i++){
+//
+//        }
+//    }
+
+
+    //find factorial of give number
+    static long findFactorial(int N){
+        long fact = 1;
+        if(N==1 || N==0){
+            return 1;
+        }
+        for(int i=2;i<=N;i++){
+            fact *=i;
+        }
+        return fact;
+    }
     public static void main(String[] args) {
         Loops_Test O = new Loops_Test();
+
+        //prime
         System.out.println("8 is prime? "+O.findPrimeorNot(8));
         System.out.println("34 is prime? "+O.findPrimeorNot(34));
         System.out.println("7 is prime? "+O.findPrimeorNot(7));
         System.out.println("137 is prime? "+O.findPrimeorNot(137));
         System.out.println("5561 is prime? "+O.findPrimeorNot(5561));
+        System.out.println("1 is prime? " + O.findPrimeorNot(1));
+
+
+        //factorial
+
+        System.out.println("\n5! = "+findFactorial(5));
+        System.out.println("1! = "+findFactorial(1));
+        System.out.println("0! = "+findFactorial(0));
+        System.out.println("10! = "+findFactorial(10));
+        System.out.println("555367! = "+findFactorial(555367));
     }
 }
