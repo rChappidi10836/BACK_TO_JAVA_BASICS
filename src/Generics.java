4d3b4b45a8f4d981a4c686f85dce9989e4aab50e
rChappidi10836 <rajasrinivas1596@gmail.com>
2026-07-01 22:53:32 +0530

    without raw usage proper usage of Generics

--- src/Generics.java
+++ src/Generics.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class game_start<T,K> {
 
 
@@ -12,13 +16,19 @@ class game_start<T,K> {
 
 }
 
+class other{
+
+    public <T> void trial_only_method(T hero){
+        System.out.println(hero);
+    }
+}
 public class Generics{
 
     public static void main(String[] args) {
-        game_start o = new game_start();
+        game_start<Integer, String> o = new game_start<>();//actully to do is game_start o = new game_start();
 
         System.out.println("Passing string to the generic class method");
-        o.letsseewhatitreturns("Hello");
+        o.letsseewhatitreturns(55); // but this will not be error then  o.letsseewhatitreturns("Hello");
 
         System.out.println("\n\nNOW INTERGER");
         o.letsseewhatitreturns(23);
@@ -29,7 +39,22 @@ public class Generics{
 
 
         System.out.println("\n\nWith 2 types of parameters");
-        o.letsseewhatitreturns(69,96);
+        o.letsseewhatitreturns(69,"hello"); // even we can do this  o.letsseewhatitreturns(69,96);
+
+        //wildCards
+        List<?> wildCard;
+        wildCard= new ArrayList<String>();
+
+        wildCard = new LinkedList<>();
+
+        System.out.println(wildCard);
+
+
+        //purely method generics
+        new other().trial_only_method("mazza ajaeyga");
+
+        //String s = new String(); used to see String class
+
     }
 
 }
